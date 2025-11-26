5c9f3c7eeef7cc2b46291a0c2c4db877e4718aed
oksanA016 <frunzeoksana86@gmail.com>
2025-11-26 14:37:02 +0200

    do-hw11

---
+++ src/js/render-functions.js
@@ -0,0 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const loader = document.querySelector(".loader");
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      img => `
+      <a href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" alt="${img.tags}" />
+      </a>
+    `
+    )
+    .join("");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+export function showLoader() {
+  loader.classList.remove("is-hidden");
+}
+
+export function hideLoader() {
+  loader.classList.add("is-hidden");
+}
+
